8ae74b2416130082d00db6f1323198cc72c4291e
Alexander282 <alexjasiil@gmail.com>
2023-02-21 19:00:06 +0200

    Base game file added

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
+# Start of Setup
 import pygame
-
-screen =  pygame.display.set_mode((600, 600))
-
 pygame.init()
-
+screen =  pygame.display.set_mode((600, 600))
 running = True
+# End of Setup
+
 while running:
+    pygame.display.update()
     for event in pygame.event.get():
+
+
+
+    # Start of QUIT
         if event.type == pygame.QUIT:
             running = False
-
 pygame.quit()
+# End of QUIT
