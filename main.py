51d15906d09eb0e835a34fa1ccce945988f0c3e3
Alexander282 <alexjasiil@gmail.com>
2023-02-21 19:09:23 +0200

    Very basic coding, based on examples shown in class

--- main.py
+++ main.py
@@ -5,8 +5,19 @@ screen =  pygame.display.set_mode((600, 600))
 running = True
 # End of Setup
 
+class ruut:
+    def __init__(self, side_length):
+        self.side_length = side_length
+
+x = 0
 while running:
-    pygame.display.update()
+    #               (where, (colour R,G,B), (xpos, ypos, xsize, ysize)
+    if x > 60:
+        x = 0
+    else:
+        x+=1
+    screen.fill((0,0,0))
+    pygame.draw.rect(screen, (255, 100, 100), (x, 30, 60, 60))
     for event in pygame.event.get():
 
 
@@ -14,5 +25,7 @@ while running:
     # Start of QUIT
         if event.type == pygame.QUIT:
             running = False
+    # 1 frame
+    pygame.display.update()
 pygame.quit()
 # End of QUIT
