52f4f01a3e6ddd290c54dbb7e7d7d2cffdebd4d0
Kennet Gabriel Kadarpik <kennet.kadarpik@kool.nrg.edu.ee>
2023-03-28 19:14:34 +0300

    doing stuff

--- main.py
+++ main.py
@@ -1,57 +1,52 @@
-# Start of Setup
+import time
 import pygame
-pygame.init()
-screen_width = 600
-screen_height = 600
-screen =  pygame.display.set_mode((screen_width, screen_height))
-running = True
-# End of Setup
-pygame.display.set_caption("LAIser")
-class ruut:
-    def __init__(self, side_length):
-        self.side_length = side_length
+
+def main():
+
+    pygame.init()    # Prepare the PyGame module for use
+    main_surface = pygame.display.set_mode((600, 600))
+
+    # Load an image to draw. Substitute your own.
+    # PyGame handles gif, jpg, png, etc. image types.
+
+    character = pygame.image.load("char.png")
+
+    # Create a font for rendering text
+    my_font = pygame.font.SysFont("Courier", 16)
+
+    frame_count = 0
+    frame_rate = 0
+    t0 = time.process_time()
+
+    x = 0
+    y = 0
+
+    while True:
+
+        # Look for an event from keyboard, mouse, joystick, etc.
+        ev = pygame.event.poll()
+        if ev.type == pygame.QUIT:   # Window close button clicked?
+            break                    # Leave game loop
+
+        # Do other bits of logic for the game here
+        frame_count += 1
+        if frame_count % 500 == 0:
+            t1 = time.process_time()
+            frame_rate = 500 / (t1-t0)
+            t0 = t1
+
+        main_surface.fill((184, 184, 184))
+
+        main_surface.blit(character,(x+5, y+5))
+
+        the_text = my_font.render("{1:.2f} fps".format(frame_count, frame_rate), True, (50, 50, 50))
+
+        main_surface.blit(the_text, (10, 584))
+
+        pygame.display.flip()
 
 
-#picture = pygame.image.load("folder/name.png").conver_alpha
-#screen.blit(picture, (x,y))
-
-x = 0
-y = 0
 
 
-
-
-while running:
-    #               (where, (colour R,G,B), (xpos, ypos, xsize, ysize)
-    screen.fill((128,128,128))
-
-    for xgrid in range(11):
-        pygame.draw.rect(screen, (190,190,190), (xgrid*60-3, 0, 6, 600))
-    for ygrid in range(11):
-        pygame.draw.rect(screen, (190, 190, 190), (0, ygrid * 60-3, 600, 6))
-    pygame.time.delay(100)
-    for event in pygame.event.get():
-
-
-
-    # Start of QUIT
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_a:
-                x-=60
-            elif event.key == pygame.K_d:
-                x+=60
-            elif event.key == pygame.K_w:
-                y-=60
-            elif event.key == pygame.K_s:
-                y+=60
-        elif event.type == pygame.QUIT:
-            running = False
-    if x > screen_height:
-        x -= 60
-    #elif x < 0:
-    # 1 frame
-    pygame.draw.rect(screen, (255,50,50), (x+5, y+5, 50, 50))
-    pygame.display.update()
-
-pygame.quit()
-# End of QUIT
+    pygame.quit()
+main()
