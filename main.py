43653436d128958a4eb0e38b1f2292622eb8cb1a
Alexander282 <alexjasiil@gmail.com>
2023-03-28 18:21:57 +0300

    Slight optimization

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 # Start of Setup
 import pygame
 pygame.init()
-screen =  pygame.display.set_mode((600, 600))
+screen_width = 600
+screen_height = 600
+screen =  pygame.display.set_mode((screen_width, screen_height))
 running = True
 # End of Setup
 pygame.display.set_caption("LAIser")
@@ -16,42 +18,17 @@ class ruut:
 x = 0
 y = 0
 
-def borderedSquare(x, y, r,g,b,width,height,borderSize):
-    colors = [r, g, b]
-    pygame.draw.rect(screen, (r,g,b), (x, y, width, height))
-    r2 = r-20
-    if r2<0:
-        r2 = 0
-    g2 = g - 20
-    if g2 < 0:
-        g2 = 0
-    b2 = b - 20
-    if b2 < 0:
-        b2 = 0
 
-    pygame.draw.rect(screen, (r2,g2,b2), (x, y, borderSize, height))
-    pygame.draw.rect(screen, (r2, g2, b2), (x, y, width, borderSize))
-    r3 = r + 20
-    if r3 > 255:
-        r3 = 255
-    g3 = g + 20
-    if g3 > 255:
-        g3 = 255
-    b3 = b + 20
-    if b3 > 255:
-        b3 = 255
-    pygame.draw.rect(screen, (r3,g3,b3), (x+width-borderSize, y, borderSize, height))
-    pygame.draw.rect(screen, (r3, g3, b3), (x, y+height-borderSize, width, borderSize))
 
 
 while running:
     #               (where, (colour R,G,B), (xpos, ypos, xsize, ysize)
     screen.fill((128,128,128))
 
-    for xgrid in range(10):
-        for ygrid in range(10):
-            pygame.draw.rect(screen, (190,190,190), (xgrid*60+5, ygrid*60+5, 50, 50))
-    borderedSquare(x, y, 255, 100, 100, 60, 60, 10)
+    for xgrid in range(11):
+        pygame.draw.rect(screen, (190,190,190), (xgrid*60-3, 0, 6, 600))
+    for ygrid in range(11):
+        pygame.draw.rect(screen, (190, 190, 190), (0, ygrid * 60-3, 600, 6))
     pygame.time.delay(100)
     for event in pygame.event.get():
 
@@ -69,7 +46,12 @@ while running:
                 y+=60
         elif event.type == pygame.QUIT:
             running = False
+    if x > screen_height:
+        x -= 60
+    #elif x < 0:
     # 1 frame
+    pygame.draw.rect(screen, (255,50,50), (x+5, y+5, 50, 50))
     pygame.display.update()
+
 pygame.quit()
 # End of QUIT
