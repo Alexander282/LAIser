e9df2f7847c95ae154fa3ff6d4ed30942484de43
Alexander282 <alexjasiil@gmail.com>
2023-04-04 18:46:32 +0300

    Groundwork for object system

--- main.py
+++ main.py
@@ -10,9 +10,11 @@ running = True
 pygame.display.set_caption("LAIser")
 #picture = pygame.image.load("folder/name.png").conver_alpha
 #screen.blit(picture, (x,y))
-props = {"Wood_Box":attributes.Attributes(False, True, False, ""),
-         "AI_Box":attributes.Attributes(False,False,True,""),
-         "Block":attributes.Attributes(False,False,False,"")
+props = {
+    #"name":attributes.Attributes(deadly, pushable, ai_controlled, laser_redirect)
+    "Wood_Box":attributes.Attributes(False, True, False, ""),
+    "AI_Box":attributes.Attributes(False,False,True,""),
+    "Block":attributes.Attributes(False,False,False,"")
 
 
 }
@@ -24,17 +26,56 @@ status = "menu"
 
 # statuses:
 # menu = main menu
+
+menu_buttons = {
+    # "name" : [xpos, ypos, color, width, height]
+    "play" : {"xpos": int(screen_width/2-75),
+              "ypos": int(screen_height/2-50-200),
+              "color": (155, 255, 97),
+              "highlight": (187, 255, 148),
+              "width": 150,
+              "height":100,
+              "active":False},
+    "settings" : {"xpos": int((screen_width/2-75)),
+              "ypos": int(screen_height/2-50),
+              "color": (97, 189, 255),
+              "highlight": (148, 210, 255),
+              "width": 150,
+              "height":100,
+              "active":False},
+    "quit" : {"xpos": int((screen_width/2-75)),
+              "ypos": int(screen_height/2-50+200),
+              "color": (255, 97, 97),
+              "highlight": (255, 148, 148),
+              "width": 150,
+              "height":100,
+              "active":False}
+}
+
 #   settings = settings menu
 #   select = choose a stage
 #       level = the active level
 while running:
     mousex, mousey = pygame.mouse.get_pos()
+    fps = 60
     if status == "menu":
         screen.fill((128, 128, 128))
+        for button in menu_buttons:
+            cur_but = menu_buttons[button]
+            if mousex in range(cur_but["xpos"],cur_but["xpos"]+cur_but["width"]) and mousey in range(cur_but["ypos"],cur_but["ypos"]+cur_but["height"]):
+                pygame.draw.rect(screen, cur_but["highlight"], (cur_but["xpos"], cur_but["ypos"], cur_but["width"], cur_but["height"]))
+                cur_but["active"] = True
+            else:
+                pygame.draw.rect(screen,cur_but["color"],(cur_but["xpos"], cur_but["ypos"], cur_but["width"], cur_but["height"]))
+                cur_but["active"] = False
         for event in pygame.event.get():
             if event.type == pygame.MOUSEBUTTONDOWN:
-                pygame.draw.rect(screen, (190, 190, 190), (mousex-50, mousey-50, 100,100))
-                print(mousex,mousey)
+                if menu_buttons["play"]["active"]:
+                    status = "level"
+                elif menu_buttons["settings"]["active"]:
+                    status = "settings"
+                elif menu_buttons["quit"]["active"]:
+                    running = False
             elif event.type == pygame.QUIT:
                 running = False
     elif status == "settings":
@@ -71,7 +112,8 @@ while running:
         #elif x < 0:
         # 1 frame
         pygame.draw.rect(screen, (255,50,50), (x+5, y+5, 50, 50))
+    #pygame.time.Clock.tick(60)
     pygame.display.update()
-
+    pygame.time.wait(int(1000/fps))
 pygame.quit()
 # End of QUIT
